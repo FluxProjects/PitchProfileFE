326c78ed55b298aa4b590ccacc9ee7ba6db09b97
Parrot1999 <parrot14996@gmail.com>
2022-02-17 10:30:08 +0500

    Removed compoennet from register. Removed ununed vals from login

--- src/markup/Pages/Loginpage3.js
+++ src/markup/Pages/Loginpage3.js
@@ -1,10 +1,8 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import RegisterTextInput from "../Components/RegisterTextInput";
 
 export default function Register() {
-  const [fname, setFName] = useState("");
-  const [lname, setLName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
