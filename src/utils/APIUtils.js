24e20ef3588d658f97774750a30ebc59104a02d8
RayanAbid <rayanabid12345@gmail.com>
2022-03-04 00:54:34 +0500

    url update

--- src/utils/APIUtils.js
+++ src/utils/APIUtils.js
@@ -1,3 +1,3 @@
-export const URL = "https://flux.pk:8080";
-// export const URL = "http://88.80.185.222:8080";
+// export const URL = "https://flux.pk:8080";
+export const URL = "http://88.80.185.222:8080";
 // https://flux.pk:8080
