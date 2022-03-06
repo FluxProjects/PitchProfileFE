594858fafd184ab388df1da9f0d76146528d3aee
RayanAbid <rayanabid12345@gmail.com>
2022-03-06 17:16:55 +0500

    push

--- src/utils/APIUtils.js
+++ src/utils/APIUtils.js
@@ -1,7 +1,7 @@
 // Heroku link
-// export const URL = "https://pitchprofile-backend.herokuapp.com";
+export const URL = "https://pitchprofile-backend.herokuapp.com";
 // Flux link
 // export const URL = "http://88.80.185.222:8080";
 
 // Localhost
-export const URL = "http://localhost:8080";
+// export const URL = "http://localhost:8080";
