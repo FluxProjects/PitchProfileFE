5986a8e24ea461a41d852d4d2d8dd608cd067ccb
RayanAbid <rayanabid12345@gmail.com>
2022-06-01 12:19:55 +0500

    url update

--- src/utils/APIUtils.js
+++ src/utils/APIUtils.js
@@ -1,7 +1,7 @@
 // Heroku link
-// export const URL = "https://pitchprofile.herokuapp.com";
+export const URL = "https://pitchprofile.herokuapp.com";
 // Localhost
-export const URL = "http://localhost:8080";
+// export const URL = "http://localhost:8080";
 export const cloudURL = "https://api.cloudinary.com/v1_1/flux-official";
 
 // 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
