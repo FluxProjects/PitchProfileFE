d3264a2c2febc86af7c1dd76d78964694375ded0
Ahsan Shakeel <ahsanshakeel@live.co.uk>
2023-01-04 01:23:04 +0500

    Update APIUtils.js

--- src/utils/APIUtils.js
+++ src/utils/APIUtils.js
@@ -1,14 +1,2 @@
-// Heroku link
 export const URL = "https://pitchprofilebe-production.up.railway.app";
-// Localhost
-// export const URL = "http://localhost:8080";
 export const cloudURL = "https://api.cloudinary.com/v1_1/flux-official";
-
-// email
-//  d-50b56a8a869445009ed9912d8655e23e {{name}}
-
-// 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
-
-// export const URL = "https://pitchprofile-backend.herokuapp.com";
-// Flux link
-// export const URL = "http://88.80.185.222:8080";
