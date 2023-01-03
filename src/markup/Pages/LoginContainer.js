4f0fab7a5ff038ecdea4668e35503286bcff6636
Ahsan Shakeel <ahsanshakeel@live.co.uk>
2023-01-04 02:16:06 +0500

    Update LoginContainer.js

--- src/markup/Pages/LoginContainer.js
+++ src/markup/Pages/LoginContainer.js
@@ -12,23 +12,18 @@ export default function LoginContainer() {
   const state = useSelector((state) => state);
   const dispatch = useDispatch();
   const history = useHistory();
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
   const [isCurrentTab, setIsCurrentTab] = useState("login");
-
   const callLoginUser = async () => {
     await dispatch(LoginUser(email.toLowerCase(), password, history));
-    // router.push("/registration/");
   };
-
   const textInputFields = [
     {
       name: "email",
       required: "true",
       className: "form-control ",
-      placeholder: "Your email",
+      placeholder: "Your Email",
       type: "text",
       label: "Email",
       onChange: (e) => {
@@ -40,7 +35,7 @@ export default function LoginContainer() {
       name: "password",
       required: "true",
       className: "form-control ",
-      placeholder: "Your password",
+      placeholder: "Your Password",
       type: "password",
       label: "Password",
       onChange: (e) => {
@@ -52,14 +47,12 @@ export default function LoginContainer() {
 
   return (
     <>
-      {/* Section: Design Block */}
       <section className="background-radial-gradient bgImageReg overflow-hidden">
         <HeaderOffline fromLogin={true} />
         <div className="container px-4 py-5 px-md-5 text-center text-lg-start ">
           <div className="row gx-lg-5 align-items-center mb-5">
             <div className="col-lg-6 mb-5 mb-lg-0" style={{ zIndex: 10 }}>
               <img src={require("../../images/networkImg.png")} />
-
               <h1
                 className="my-3 text-left display-5 fw-bold ls-tight"
                 style={{ color: "blue" }}
@@ -98,12 +91,6 @@ export default function LoginContainer() {
               </p>
             </div>
             <div className="col-lg-6 mb-5 mb-lg-0 p-0 position-relative">
-              {/* <p className="font-weight-600 text-white text-center">
-                <Link className="text-white" to={"/company-login"}>
-                  Login as a comapny
-                </Link>
-                .
-              </p> */}
               <div
                 className="card "
                 style={{
@@ -137,59 +124,6 @@ export default function LoginContainer() {
         </div>
         <Footer />
       </section>
-      {/* Section: Design Block */}
     </>
-
-    // <div className="page-content bg-gray login-form-bx browse-job login-style2">
-    //   <div className="section-full">
-    //     <div className="container-fluid">
-    //       <div className="row">
-    //         <div className="col-lg-6 col-md-7 box-skew ">
-    //           {/* Container left */}
-    //         </div>
-    //         <div className="col-lg-6 col-sm-12 col-xs-12 col-md-5 d-flex ">
-    //           {/* Container right */}
-    // <p className="font-weight-600 text-center">
-    //   <Link to={"/company-login"}>Login as a comapny</Link>.
-    // </p>
-    // {isCurrentTab == "login" ? (
-    //   <Loginpage3
-    //     isCurrentTab={isCurrentTab}
-    //     setIsCurrentTab={(e) => {
-    //       console.log("test", e);
-    //       setIsCurrentTab(e);
-    //     }}
-    //   />
-    // ) : (
-    //   <Register
-    //     isCurrentTab={isCurrentTab}
-    //     setIsCurrentTab={(e) => {
-    //       console.log("test", e);
-    //       setIsCurrentTab(e);
-    //     }}
-    //   />
-    // )}
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   <footer className="login-footer">
-    //     <div className="container">
-    //       <div className="row">
-    //         <div className="col-lg-12 text-center">
-    //           {/* <span className="float-left text-black-light">
-    //             {" "}
-    //             © Copyright by{" "}
-    //             <i className="fa fa-heart m-lr5 text-red heart"></i>
-    //             <Link to={"#"} className="text-primary">
-    //               Pitch profile{" "}
-    //             </Link>
-    //           </span>
-    //           <span className="float-right">All rights reserved.</span> */}
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </footer>
-    // </div>
   );
 }
