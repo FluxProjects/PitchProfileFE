400980c57b6d89d8a43bea69a601fdc038811397
Ahsan Shakeel <ahsanshakeel@live.co.uk>
2023-01-04 02:04:46 +0500

    Update Message.js

--- src/markup/Pages/MyChat/Messages/Message/Message.js
+++ src/markup/Pages/MyChat/Messages/Message/Message.js
@@ -1,14 +1,11 @@
 import React from "react";
-
 import "./Message.css";
-
 import ReactEmoji from "react-emoji";
 import { useSelector } from "react-redux";
 import moment from "moment";
 
 const Message = ({ message }) => {
   let isSentByCurrentUser = false;
-
   const state = useSelector((state) => state);
 
   if (state.userDetails.id === message.sent_by) {
@@ -17,7 +14,6 @@ const Message = ({ message }) => {
 
   return isSentByCurrentUser ? (
     <div className="messageContainerChat justifyEndChat">
-      {/* <p className="sentTextChat pr-10Chat">{trimmedName}</p> */}
       <div className="messageBoxChat backgroundBlueChat">
         <p className="messageTextChat colorWhiteChat">
           {ReactEmoji.emojify(message.text)}
@@ -39,7 +35,6 @@ const Message = ({ message }) => {
           </span>
         </p>
       </div>
-      {/* <p className="sentText pl-10 ">{user}</p> */}
     </div>
   );
 };
