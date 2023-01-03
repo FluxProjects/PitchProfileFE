1e686c5f5ded4b46103c58dcdf6d42215e3f5cbf
Ahsan Shakeel <ahsanshakeel@live.co.uk>
2023-01-04 01:25:23 +0500

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCGVmoLiM3D1HhFzcctWhtJxI43qsUeg0Y",
   authDomain: "pitchprofile-chat.firebaseapp.com",
@@ -16,6 +11,5 @@ const firebaseConfig = {
   measurementId: "G-8WZY2K3TWG",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
